68f11e2ce57604f9d0c71fbad3eb1a262a1a7f25
nhv215 <60795677+nhv215@users.noreply.github.com>
2020-10-25 23:21:11 -0400

    added FE Features

---
+++ AppEasePlatform/frontendapp/src/components/login.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    }
+    login = event => {
+        console.log(this.state.credentials);
+        fetch('http://127.0.0.1:8000/api/login/', {
+            method: 'POST',
+            headers: 
+            {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(this.state.credentials)
+        }).then(
+            data => {
+                console.log(data);
+            }
+        ).catch(error => console.error(error))
+    }
+    inputChanged = event => {
+        const credentials = this.state.credentials;
+        credentials[event.target.name] = event.target.value;
+        this.setState({credentials: credentials})
+    }
+    render() {
+        return ( 
+            <div className = "App" >
+                <h1 > Login User </h1>
+                <label>Username:
+                    <input type='text' name='username'
+                    value={this.state.credentials.username}
+                    onChange={this.inputChanged}/>    
+                </label>
+                <br/>
+                <label>Password:
+                    <input type='password' name='password'
+                    value={this.state.credentials.password}
+                    onChange={this.inputChanged}/>    
+                </label>
+                <br/>
+                <button onClick={this.login}>Login</button>
+            </div>
+        );
+    }
+
+}
+
+export default Login;
