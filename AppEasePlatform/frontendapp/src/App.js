29aef0d60501a43eb1fb9e5233fbd44b42f14019
nhv215 <60795677+nhv215@users.noreply.github.com>
2020-10-26 00:22:22 -0400

    FE for login

---
+++ AppEasePlatform/frontendapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './components/login';
+import React from 'react';
+
+function App() {
+  return ( 
+    <div className = "App" >
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
