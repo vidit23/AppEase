4d0109ba00c22189e7e5f3cc2961d4f6d051017e
vidit23 <viditvineet@hotmail.com>
2021-01-06 10:37:16 +0400

    Different tables Kafka adding

--- kafkaConsumer.py
+++ kafkaConsumer.py
@@ -7,20 +7,27 @@ c = conn.cursor()
 
 consumer = KafkaConsumer(
     'healthData',
-     bootstrap_servers=['10.0.0.55:9092'],
+     bootstrap_servers=['192.168.0.155:9092'],
      auto_offset_reset='latest',
      value_deserializer=lambda x: x.decode('utf-8')
 )
 
-# Getting the count of tables with the name
-c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='healthData' ''')
 
-#if the count is 1, then table exists
-if c.fetchone()[0]==1 :
-    print('Table exists.')
-else :
-    print('Table doesnt exist')
-    
+def tableExists(tableName):
+    # Getting the count of tables with the name
+    queryParameters = (tableName,)
+    c.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name=?", queryParameters)
+
+    #if the count is 1, then table exists
+    if c.fetchone()[0]==1 :
+        print(tableName, ' exists.')
+        return True
+    else :
+        print(tableName, ' doesnt exist')
+        return False
+        
+
+if not tableExists("healthData"):
     c.execute('''create table healthData (
     timeStamp text, userToken text, age integer, sex text, bloodType text,
     heartRate integer, stepsCount integer, distanceCovered real, 
@@ -28,20 +35,51 @@ else :
     runningLabelCount integer, automotiveLabelCount integer, 
     cyclingLabelCount integer, unknownLabelCount integer)''')
 
-    # Add to DB if table doesnt exist
     conn.commit()
-    print('Created the table')
+    print('Created the health table')
+
+if not tableExists("locationData"):
+    c.execute('''create table locationData (timeStamp text, userToken text, longitude real, latitude real)''')
+    conn.commit()
+    print('Created the location table')
+
+if not tableExists("gameLogData"):
+    c.execute('''create table gameLogData (timeStamp text, userToken text, gameStatus text, gameName text)''')
+    conn.commit()
+    print('Created the game log table')
+
 
 for message in consumer:
     message = message.value
     try: 
         message = loads(message)
         print(message)
-        columns = ', '.join(message.keys())
-        placeholders = ':'+', :'.join(message.keys())
-        query = 'INSERT INTO healthData (%s) VALUES (%s)' % (columns, placeholders)
-        c.execute(query, message)
-        conn.commit()
+        if "gameStatus" in message.keys():
+            columns = ', '.join(message.keys())
+            placeholders = ':'+', :'.join(message.keys())
+            query = 'INSERT INTO gameLogData (%s) VALUES (%s)' % (columns, placeholders)
+            c.execute(query, message)
+            conn.commit()
+        else:
+            locationMessage = {
+                "timeStamp": message["timeStamp"],
+                "userToken": message["userToken"],
+                "longitude": message["longitude"],
+                "latitude": message["latitude"]
+            }
+            columns = ', '.join(locationMessage.keys())
+            placeholders = ':'+', :'.join(locationMessage.keys())
+            query = 'INSERT INTO locationData (%s) VALUES (%s)' % (columns, placeholders)
+            c.execute(query, locationMessage)
+
+
+            message.pop('longitude', None)
+            message.pop('latitude', None)
+            columns = ', '.join(message.keys())
+            placeholders = ':'+', :'.join(message.keys())
+            query = 'INSERT INTO healthData (%s) VALUES (%s)' % (columns, placeholders)
+            c.execute(query, message)
+            conn.commit()
     except:
         print("Couldnt convert to json")
 
